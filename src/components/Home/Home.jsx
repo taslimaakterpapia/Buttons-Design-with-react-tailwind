694e477232009149cea3c81a01064f568ccf1942
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-11-02 23:38:12 +0600

    button 4 done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Button1 from "../Buttons/Button1"; 
 import Button2 from "../Buttons/Button2";
 import Button3 from "../Buttons/Button3";
+import Button4 from "../Buttons/Button4";
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Button1></Button1>
             <Button2></Button2>
             <Button3></Button3>
+            <Button4></Button4>
             </div>
         </div>
     );
