c0d3c90ebb77c4e1c552d1be215a522872841a83
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-11-04 02:47:13 +0600

    button serial done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,39 @@
-import Button1 from "../Buttons/Button1"; 
-import Button2 from "../Buttons/Button2";
-import Button3 from "../Buttons/Button3";
-import Button4 from "../Buttons/Button4";
-import Button5 from "../Buttons/Button5";
+
+
+
+import GitBtn1 from "../Buttons/Github/GitBtn1";
+import GitBtn2 from "../Buttons/Github/GitBtn2";
+import GitBtn3 from "../Buttons/Github/GitBtn3";
+import SavePostBtn1 from "../Buttons/SavePost/SavePostBtn1";
+
 
 const Home = () => {
     return (
-        <div className="bg-neutral-800 py-10">            
-            <h1 className="text-center text-white text-5xl font-bold pb-20 ">Button Design </h1>
+        <div className="bg-neutral-800 py-10 px-10">            
+            <h1 className="text-center text-white text-5xl font-bold pb-20 underline">Button Design </h1>
+            
+            
+            <h3 className="text-2xl text-white font-semibold pb-10 underline">GitHub Buttons</h3>
 
             <div className="flex gap-16  items-center">
-            <Button1></Button1>
-            <Button2></Button2>
-            <Button3></Button3>
-            <Button4></Button4>
-            <Button5></Button5>
+
+            <GitBtn1></GitBtn1>
+            <GitBtn2></GitBtn2>
+            <GitBtn3></GitBtn3>
+
+            
+           
+            </div>
+
+
+
+
+            <h3 className="text-2xl text-white font-semibold pt-20 pb-10 underline">Save Post Buttons</h3>
+
+            <div className="flex gap-16  items-center pb-20">
+
+            <SavePostBtn1></SavePostBtn1>
+           
             </div>
         </div>
     );
